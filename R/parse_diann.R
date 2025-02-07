07f527b0b09e553bc215018c1f68bba1adfcb903
Roman-Si <roman_siap@hotmail.com>
2025-02-07 17:01:47 +0200

    First test of diann changes

---
+++ R/parse_diann.R
@@ -0,0 +1,72 @@
+#' @import dplyr
+#' @importFrom arrow read_parquet
+NULL
+
+#' Convert diann parquet to msstats_in format
+#'
+#' Converts the diann parquet to msstats_in format after filtering for contaminants and qvalue of precursors, then renames columns to prepare dataframe for functions like the extract_peptides_per_protein
+#'
+#' @param diann_parquet Path to diann parquet main report
+#' @param qvalue FDR for precursors (default 0.01)
+#' @param contaminant_prefix Prefix for contaminants (default is Cont)
+#' @return Data frame with reliable precursors and without contaminants and the following columns for msstats_input format
+# - `ProteinName`: derived from the Protein.Group column
+# - `PeptideSequence`: derived from the Stripped.Sequence column
+#' @export
+convert_diann_to_msstats_in <- function(diann_parquet, qvalue = 0.01, contaminant_prefix = "Cont") {
+    diann_df <- arrow::read_parquet(diann_parquet)
+    diann_df <- diann_df %>%
+        filter(!grepl(contaminant_prefix, Protein.Ids)) %>%
+        filter(Q.Value <= 0.01)
+    diann_df$ProteinName <- diann_df$Protein.Group
+    diann_df$PeptideSequence <- diann_df$Stripped.Sequence
+  return(diann_df)
+}
+
+
+#' Prepare diann for prolfqua
+#'
+#' Converts the diann parquet to a protein-level dataframe that will be converted to prolfqua object with create_lfqdata function
+#'
+#' @param diann_parquet Path to diann parquet main report
+#' @param contaminant_prefix Prefix for contaminants (default is Cont)
+#' @param response_level Specifies the level to use:
+#'   - `"protein"` (default): Will use the proteinGroup level MaxLFQ
+#'   - `"gene"`: Will use the gene level MaxLFQ
+#' @param qvalue FDR for precursors and response level (default 0.01)
+#' @return Data frame with reliable precursors and without contaminants and the following columns for msstats_input format
+# - `accession`: derived from the Protein.Group column
+# - `PeptideSequence`: derived from the Stripped.Sequence column
+#' @export
+prepare_diann_for_prolfqua <- function(diann_parquet, qvalue = 0.01, contaminant_prefix = "Cont") {
+    diann_df <- arrow::read_parquet(diann_parquet)
+
+    # Filter for contaminants and precursors 1% FDR
+    diann_df <- diann_df %>%
+        filter(!grepl(contaminant_prefix, Protein.Ids)) %>%
+        filter(Q.Value <= 0.01)
+        
+    # check the response level is correct
+    if (!response_level %in% c("protein", "gene")) {
+        stop("Invalid response_level. Choose either 'protein' or 'gene'.")
+        }
+
+    if (response_level == "protein") {
+        quant_df <- diann_df %>% 
+                        filter(PG.Q.Value <= 0.01) %>% 
+                        select(Run, Protein.Group, PG.MaxLFQ) %>%
+                        distinct()
+        }
+    
+    if (response_level == "gene") {
+        quant_df <- diann_df %>% 
+                        filter(GG.Q.Value <= 0.01) %>% 
+                        select(Run, Genes, Genes.MaxLFQ) %>% 
+                        distinct()
+        }
+    colnames(quant_df) <- c("Reference", "ProteinName", "Intensity")
+
+    diann_df$accession <- diann_df$Protein.Group
+    diann_df$PeptideSequence <- diann_df$Stripped.Sequence
+  return(diann_df)
+}
--- R/parse_msstats_in.R
+++ R/parse_msstats_in.R
@@ -3,50 +3,48 @@
 #' @importFrom Biostrings readAAStringSet
 NULL
 
-#' Extract peptide sequences, count and indices per protein
-#'
-#' Processes the msstats_in (formatted as input for msstats) file to extract peptide sequences, number and indexes of quantified peptides for each protein. Maybe too long and needs splitting
+
+#' Preprocess msstats_in by removing contaminants and stripping PTMs from peptide sequence for function extract_peptides_per_protein
 #'
 #' @param msstats_input_path Path to msstats input style file from quantms (probably works for other programs as well?)
-#' @param fasta_path Path to fasta file with protein sequences (can be gzipped)
 #' @param contaminant_prefix Prefix for contaminants (default is CONTAMINANT)
+#' @return Data frame without contaminants and with column PeptideSequence stripped from PTMs
+#' @export
+strip_ptms_from_msstats_in <- function(msstats_input_path, contaminant_prefix = "CONTAMINANT") {
+    msstats_in_df <- read.csv(msstats_input_path, header = TRUE, sep = ',')
+    msstats_in_df <- msstats_in_df %>%
+      filter(!grepl(contaminant_prefix, ProteinName)) %>%
+      mutate(PeptideSequence = stringr::str_replace_all(PeptideSequence, "\\(.*?\\)", "") %>% # Remove modifications
+               stringr::str_replace_all("\\.", "")) # Remove dots
+    return(msstats_in_df)
+}
+
+
+
+#' Extract peptide sequences, count and indices per protein
+#'
+#' Processes the msstats_in_df dataframe to extract peptide sequences, number and indexes of quantified peptides for each protein
+#'
+#' @param msstats_in_df msstats_input style dataframe with PeptideSequence column stripped from PTMs and proteinId in column ProteinName
+#' @param fasta_path Path to fasta file with protein sequences (can be gzipped)
 #' @param delimiter Delimiter for peptides shared between different proteins (default is ;)
-#' @param output_file Optional. Name of the output file (better to merge it with the output of parse_mzTab::extract_protein_stats first and save together)
 #' @return Data frame with peptide info (without PTMs) for each detected protein including:
 # - `peptide_seqs`: ; delimited peptide sequences
 # - `nr_unmodified_peptides`: count of unmodified peptides
 # - `peptide_index`: min and max index of identified peptides in protein sequence, useful to check which part of the protein is detected
 #' @details This function performs the following steps:
-#'   - Reads the msstats input file and removes PTM information from peptide sequences
 #'   - Filters the FASTA file to include only detected proteins from msstats_in
 #'   - Extracts peptide sequences, counts, and positional indices for each protein.
 #' @export
-extract_peptides_per_protein <- function(msstats_input_path, fasta_path, contaminant_prefix = "CONTAMINANT", delimiter = ";", output_file = NULL) {
+extract_peptides_per_protein <- function(msstats_df, fasta_path, delimiter = ";", output_file = NULL) {
   
-  # Helper function to preprocess msstats input
-  preprocess_msstats_input <- function(msstats_input_path, contaminant_prefix) {
-    msstats_input <- read.csv(msstats_input_path, header = TRUE, sep = ',')
-    msstats_input <- msstats_input %>%
-      filter(!grepl(contaminant_prefix, ProteinName)) %>%
-      mutate(PeptideSequence = stringr::str_replace_all(PeptideSequence, "\\(.*?\\)", "") %>% # Remove modifications
-               stringr::str_replace_all("\\.", "")) # Remove dots
-    return(msstats_input)
-  }
-
-  # Helper function to filter the FASTA file for detected proteins (move it in general functions?)
-  filter_fasta_file <- function(fasta_path, protein_ids) {
-    fasta_file <- readAAStringSet(fasta_path, format = "fasta")
-    fasta_file <- fasta_file[names(fasta_file) %in% protein_ids]
-    return(fasta_file)
-  }
   
   # Read and preprocess inputs
-  msstats_input <- preprocess_msstats_input(msstats_input_path, contaminant_prefix)
   protein_ids <- unlist(strsplit(msstats_input$ProteinName, delimiter)) %>% unique()
-  fasta_file <- filter_fasta_file(fasta_path, protein_ids)
+  fasta_file <- filter_fasta_file_with_ids(fasta_path, protein_ids)
 
   # Initialize peptide_df
-  peptide_df <- data.frame(accession = protein_ids, nr_unmodified_peptides = NA,  peptide_index = NA, peptide_seqs = NA, stringsAsFactors = FALSE)
+  peptide_df <- data.frame(protein_Id = protein_ids, nr_unmodified_peptides = NA,  peptide_index = NA, peptide_seqs = NA, stringsAsFactors = FALSE)
 
   # Iterate over proteins in fasta
   for (i in seq_along(fasta_file)) {
@@ -68,8 +66,8 @@ extract_peptides_per_protein <- function(msstats_input_path, fasta_path, contami
       }
 
     # Update peptide_df
-    peptide_df[peptide_df$accession == protein_id, ] <- list(
-      accession = protein_id,
+    peptide_df[peptide_df$protein_Id == protein_id, ] <- list(
+      protein_Id = protein_id,
       nr_unmodified_peptides = length(peptides),
       peptide_index = paste0(start_index, "-", end_index),
       peptide_seqs = paste(peptides, collapse = delimiter)
@@ -77,11 +75,6 @@ extract_peptides_per_protein <- function(msstats_input_path, fasta_path, contami
     )
   }
   
-  
-  # if save to csv
-  if (!is.null(output_file)) {
-    save_gzipped_csv(peptide_df, output_file)
-  }
   
   return(peptide_df)
 }
